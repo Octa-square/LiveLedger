a34b387cb9a4ad37ee001744bd4042b67bf018d6
chinonso <chinonsochiji@gmail.com>
2025-12-02 20:44:55 -0500

    Initial Commit

---
+++ SalesApp/SalesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesAppApp.swift
+//  SalesApp
+//
+//  Created by Chinonso Chiji on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SalesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SalesAppTests/SalesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalesAppTests.swift
+//  SalesAppTests
+//
+//  Created by Chinonso Chiji on 12/2/25.
+//
+
+import Testing
+@testable import SalesApp
+
+struct SalesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
